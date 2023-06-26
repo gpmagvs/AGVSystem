a598b298864a376af2850d86e43ecdc593c5f154
jinweihuang <jinweihuang2033@outlook.com>
2023-06-26 17:11:06 +0800

    add clone projects bat file

---
+++ agvsystem_projects_clone.bat
@@ -0,0 +1,7 @@
+cd ../
+git clone https://github.com/gpmagvs/AGVSystem.git
+git clone https://github.com/gpmagvs/AGVS_UI.git
+git clone https://github.com/gpmagvs/AGVSystemCommonNet6.git
+git clone https://github.com/gpmagvs/EquipmentManagment.git
+git clone https://github.com/gpmagvs/RosBridgeClient.git
+pause
---
+++ agvsystem_projects_clone_gitlab.bat
@@ -0,0 +1,7 @@
+cd ../
+git clone http://gitlab.gpmcorp.com.tw/smart_agv/AGVSystem.git
+git clone http://gitlab.gpmcorp.com.tw/smart_agv/AGVS_UI.git
+git clone http://gitlab.gpmcorp.com.tw/smart_agv/AGVSystemCommonNet6.git
+git clone http://gitlab.gpmcorp.com.tw/smart_agv/EquipmentManagment.git
+git clone http://gitlab.gpmcorp.com.tw/smart_agv/RosBridgeClient.git
+pause
