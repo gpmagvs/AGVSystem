bd54f72e6c9d6f27954a3bb77cae59d34a04f4c0
Jin-wei Huang <ss610349@gmail.com>
2023-06-26 17:12:18 +0800

    Update agvsystem_projects_clone_gitlab.bat

--- agvsystem_projects_clone_gitlab.bat
+++ agvsystem_projects_clone_gitlab.bat
@@ -1,5 +1,4 @@
 cd ../
-git clone http://gitlab.gpmcorp.com.tw/smart_agv/AGVSystem.git
 git clone http://gitlab.gpmcorp.com.tw/smart_agv/AGVS_UI.git
 git clone http://gitlab.gpmcorp.com.tw/smart_agv/AGVSystemCommonNet6.git
 git clone http://gitlab.gpmcorp.com.tw/smart_agv/EquipmentManagment.git
