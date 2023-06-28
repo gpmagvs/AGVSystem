eb35add835335c3fa9322b84c4e3a7da5bcef1ba
jinweihuang <jinweihuang1020@gmail.com>
2023-06-28 22:38:47 +0800

    Modify agvsystem_projects_clone.bat add git clone https://github.com/gpmagvs/VMSystem.git

--- agvsystem_projects_clone.bat
+++ agvsystem_projects_clone.bat
@@ -3,4 +3,5 @@ git clone https://github.com/gpmagvs/AGVS_UI.git
 git clone https://github.com/gpmagvs/AGVSystemCommonNet6.git
 git clone https://github.com/gpmagvs/EquipmentManagment.git
 git clone https://github.com/gpmagvs/RosBridgeClient.git
+git clone https://github.com/gpmagvs/VMSystem.git
 pause
