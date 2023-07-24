c9b98c06fbd94c15e6fe57c94574e488ef186b27
stanleytsai <stanleytsai@gpmcorp.com.tw>
2023-07-24 16:54:59 +0800

    Query功能

---
+++ Controllers/AlarmqueryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLite;
+using AGVSystemCommonNet6.Alarm;
+using AGVSystemCommonNet6.DATABASE;
+using System.Collections.Generic;
+
+namespace AGVSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlarmqueryController : ControllerBase
+    {
+        [HttpGet("Query")]
+        public async Task<IActionResult> Query(int currentpage, string StartTime, string EndTime, string? AGV_Name = "ALL")
+        {
+            DateTime start = DateTime.Parse(StartTime);
+            DateTime end = DateTime.Parse(EndTime);
+            AlarmManagerCenter.Query(out int count, currentpage,start, end, AGV_Name, out List<clsAlarmDto> ? alarms);
+            return Ok(new { count,alarms });
+        }
+        public class Alarmquery_options
+        {
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+
+            public string AGV_Name { get; set; } = "ALL";
+        }
+
+    }
+}
