45a90c6ee260869276b7f35d42a0c7c4f4b6c63b
jinweihuang <jinweihuang2033@outlook.com>
2024-02-04 09:12:52 +0800

    Add ChargeStationEmuController 方便模擬充電站

---
+++ Controllers/EmuControllers/ChargeStationEmuController.cs
@@ -0,0 +1,26 @@
+﻿using EquipmentManagment.Manager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGVSystem.Controllers.EmuControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChargeStationEmuController : ControllerBase
+    {
+        [HttpGet("NoAGVCharging")]
+        public async Task<IActionResult> NoAGVCharging()
+        {
+            var charge_station = StaEQPEmulatorsManagager.ChargeStationEmulators.FirstOrDefault().Value;
+            charge_station.AGVNoCharging();
+            return Ok();
+        }
+        [HttpGet("AGVCharging")]
+        public async Task<IActionResult> AGVCharging()
+        {
+            var charge_station = StaEQPEmulatorsManagager.ChargeStationEmulators.FirstOrDefault().Value;
+            charge_station.AGVCharging();
+            return Ok();
+        }
+    }
+}
