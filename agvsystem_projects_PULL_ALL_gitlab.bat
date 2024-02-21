6fbe50731c87d894b33603e0188501766c78767a
PoJen <pojenchao@gpmcorp.com.tw>
2024-02-21 15:47:26 +0800

    agvsystem_projects_PULL_ALL_gitlab.bat增加GPMVehicleControlSystem及vmsystem的pull

--- agvsystem_projects_PULL_ALL_gitlab.bat
+++ agvsystem_projects_PULL_ALL_gitlab.bat
@@ -27,5 +27,17 @@ call git checkout master
 call git pull origin master 
 cd ../
 
+cd GPMVehicleControlSystem
+call git fetch
+call git checkout master
+call git pull origin master 
+cd ../
+
+cd vmsystem
+call git fetch
+call git checkout master
+call git pull origin master 
+cd ../
+
 
 pause
