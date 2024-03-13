3ec0aa0d84d430bd08b1cee8086c4f6c4a26b4f4
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-03-13 12:28:54 +0800

    修改設備可用車輛的列舉名稱

--- TaskManagers/Extension.cs
+++ TaskManagers/Extension.cs
@@ -7,24 +7,24 @@ namespace AGVSystem.TaskManagers
     public static class Extension
     {
 
-        public static EquipmentManagment.Device.Options.AGV_TYPE ConvertToEQAcceptAGVTYPE(this clsEnums.AGV_TYPE agv_type)
+        public static EquipmentManagment.Device.Options.VEHICLE_TYPE ConvertToEQAcceptAGVTYPE(this clsEnums.AGV_TYPE agv_type)
         {
             switch (agv_type)
             {
-                case clsEnums.AGV_TYPE.FORK: return EquipmentManagment.Device.Options.AGV_TYPE.FORK;
-                case clsEnums.AGV_TYPE.SUBMERGED_SHIELD: return EquipmentManagment.Device.Options.AGV_TYPE.SUBMERGED_SHIELD;
-                default: return EquipmentManagment.Device.Options.AGV_TYPE.ALL;
+                case clsEnums.AGV_TYPE.FORK: return EquipmentManagment.Device.Options.VEHICLE_TYPE.FORK;
+                case clsEnums.AGV_TYPE.SUBMERGED_SHIELD: return EquipmentManagment.Device.Options.VEHICLE_TYPE.SUBMERGED_SHIELD;
+                default: return EquipmentManagment.Device.Options.VEHICLE_TYPE.ALL;
             }
         }
 
 
-        public static clsEnums.AGV_TYPE ToAGVModel(this EquipmentManagment.Device.Options.AGV_TYPE agv_type)
+        public static clsEnums.AGV_TYPE ToAGVModel(this EquipmentManagment.Device.Options.VEHICLE_TYPE agv_type)
         {
             switch (agv_type)
             {
-                case EquipmentManagment.Device.Options.AGV_TYPE.FORK:
+                case EquipmentManagment.Device.Options.VEHICLE_TYPE.FORK:
                     return clsEnums.AGV_TYPE.FORK;
-                case EquipmentManagment.Device.Options.AGV_TYPE.SUBMERGED_SHIELD:
+                case EquipmentManagment.Device.Options.VEHICLE_TYPE.SUBMERGED_SHIELD:
                     return clsEnums.AGV_TYPE.SUBMERGED_SHIELD;
                 default:
                     return clsEnums.AGV_TYPE.FORK;
