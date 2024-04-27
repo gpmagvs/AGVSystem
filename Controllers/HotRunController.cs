8c0cc460e25fe1c8df68b535d2a18ad667510d4a
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-04-27 09:20:59 +0800

    將 hotrun 相關 API 另新增控制器管理

---
+++ Controllers/HotRunController.cs
@@ -0,0 +1,39 @@
+﻿using AGVSystem.Models.TaskAllocation.HotRun;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGVSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotRunController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SaveHotRun([FromBody] HotRunScript[] settings)
+        {
+            return Ok(new
+            {
+                result = HotRunScriptManager.Save(settings),
+                message = ""
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHotRunScripts()
+        {
+            return Ok(HotRunScriptManager.HotRunScripts);
+        }
+        [HttpGet("Start")]
+        public async Task<IActionResult> StartHotRun(int no)
+        {
+            (bool confirm, string message) response = HotRunScriptManager.Run(no);
+            return Ok(new { confirm = response.confirm, message = response.message });
+        }
+        [HttpGet("Stop")]
+        public async Task<IActionResult> StopHotRun(int no)
+        {
+            HotRunScriptManager.Stop(no);
+            return Ok();
+        }
+    }
+}
