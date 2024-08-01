50e89b5385ff7cd0b50ec552dd52035345619baf
jinweihuang20 <jinweihuang1020@gmail.com>
2024-08-01 09:43:23 +0800

    EQDeviceEventsHandler.MaintainEvents.cs 非同步方法(ChangeEnableStateOfEntryPointOfEQOfMapAndRequestVMSReload)卡控任務併發/修改LOG實體

--- Models/EQDevices/EQDeviceEventsHandler.MaintainEvents.cs
+++ Models/EQDevices/EQDeviceEventsHandler.MaintainEvents.cs
@@ -1,30 +1,27 @@
 ﻿using AGVSystem.Models.Map;
-using AGVSystem.Service;
 using AGVSystemCommonNet6.Configuration;
-using AGVSystemCommonNet6.Log;
 using AGVSystemCommonNet6.MAP;
 using AGVSystemCommonNet6.Microservices;
-using AGVSystemCommonNet6.Microservices.VMS;
 using AGVSystemCommonNet6.Notify;
 using EquipmentManagment.Device;
-using System.Diagnostics;
 
 namespace AGVSystem.Models.EQDevices
 {
     public partial class EQDeviceEventsHandler
     {
         private static bool _disableEntryPointWhenEQMaintaining => AGVSConfigulator.SysConfigs.EQManagementConfigs.DisableEntryPointWhenEQMaintaining;
-
+        private static SemaphoreSlim _semaphoreSlim = new(1, 1);
         private static void HandleDeviceMaintainFinish(object? sender, EndPointDeviceAbstract device)
         {
-            LOG.TRACE($"{device.EQName} Maintain Signal OFF");
+            _logger.Trace($"{device.EQName} Maintain Signal OFF");
             NotifyServiceHelper.SUCCESS($"設備-{device.EQName} 完成維修");
-            ChangeEnableStateOfEntryPointOfEQOfMapAndRequestVMSReload(device.EndPointOptions.TagID, true);
+            if (_disableEntryPointWhenEQMaintaining)
+                ChangeEnableStateOfEntryPointOfEQOfMapAndRequestVMSReload(device.EndPointOptions.TagID, true);
         }
 
         private static void HandleDeviceMaintainStart(object? sender, EndPointDeviceAbstract device)
         {
-            LOG.TRACE($"{device.EQName} Maintain Signal ON");
+            _logger.Trace($"{device.EQName} Maintain Signal ON");
             NotifyServiceHelper.WARNING($"設備-{device.EQName} 維修中!");
             if (_disableEntryPointWhenEQMaintaining)
                 ChangeEnableStateOfEntryPointOfEQOfMapAndRequestVMSReload(device.EndPointOptions.TagID, false);
@@ -33,27 +30,38 @@ namespace AGVSystem.Models.EQDevices
 
         private static async Task ChangeEnableStateOfEntryPointOfEQOfMapAndRequestVMSReload(int deviceTag, bool enabled)
         {
-
-            MapPoint deviceMapPoint = AGVSMapManager.CurrentMap.Points.Values.FirstOrDefault(pt => pt.TagNumber == deviceTag);
-            if (deviceMapPoint == null)
+            try
             {
-                LOG.WARN($"[EQDeviceEventsHandler] Cannot find Map Point that TagNumber equal {deviceTag}");
-                return;
-            }
-            IEnumerable<MapPoint> entryPoints = deviceMapPoint.Target.Keys.Select(_index => AGVSMapManager.CurrentMap.Points[_index]);
-            if (!entryPoints.Any())
-                return;
+                await _semaphoreSlim.WaitAsync();
+                MapPoint deviceMapPoint = AGVSMapManager.CurrentMap.Points.Values.FirstOrDefault(pt => pt.TagNumber == deviceTag);
+                if (deviceMapPoint == null)
+                {
+                    _logger.Warn($"[EQDeviceEventsHandler] Cannot find Map Point that TagNumber equal {deviceTag}");
+                    return;
+                }
+                IEnumerable<MapPoint> entryPoints = deviceMapPoint.Target.Keys.Select(_index => AGVSMapManager.CurrentMap.Points[_index]);
+                if (!entryPoints.Any())
+                    return;
 
-            foreach (MapPoint point in entryPoints)
-            {
-                point.Enable = enabled;
+                foreach (MapPoint point in entryPoints)
+                {
+                    point.Enable = enabled;
+                }
+                clsMapConfigs mapConfigs = AGVSConfigulator.SysConfigs.MapConfigs;
+                MapManager.SaveMapToFile(AGVSMapManager.CurrentMap, mapConfigs.MapFileFullName);
+                await NotifyServiceHelper.INFO($"Map-Point-Enabled-Property-Changed", false);
+                MapSync.SendReloadRequest(mapConfigs.CurrentMapFileName);
+                string modifiedTagCollectionsStr = string.Join(",", entryPoints.Select(pt => pt.TagNumber));
+                _logger.Trace($"Modify Tags={modifiedTagCollectionsStr} 'Enable' property to {enabled}");
             }
-            clsMapConfigs mapConfigs = AGVSConfigulator.SysConfigs.MapConfigs;
-            MapManager.SaveMapToFile(AGVSMapManager.CurrentMap, mapConfigs.MapFileFullName);
-            await NotifyServiceHelper.INFO($"Map-Point-Enabled-Property-Changed", false);
-            MapSync.SendReloadRequest(mapConfigs.CurrentMapFileName);
-            string modifiedTagCollectionsStr = string.Join(",", entryPoints.Select(pt => pt.TagNumber));
-            LOG.TRACE($"Modify Tags={modifiedTagCollectionsStr} 'Enable' property to {enabled}");
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
 
         }
 
