d9684dd063b6baff9aa2edd1fd33047e3ddd1d0f
jinweihuang20 <jinweihuang1020@gmail.com>
2024-08-13 14:30:56 +0800

    新增CIMController.cs

---
+++ Controllers/CIMController.cs
@@ -0,0 +1,18 @@
+﻿using AGVSystemCommonNet6.Microservices.MCS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGVSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CIMController : ControllerBase
+    {
+        [HttpPost("ChangePortType")]
+        public async Task<IActionResult> ChangePortType(int eqTag, int portType)
+        {
+            GPMCIMService.clsCIMResponse response = await GPMCIMService.ChangePortTypeOfEq(eqTag, portType);
+            return Ok(response);
+        }
+    }
+}
