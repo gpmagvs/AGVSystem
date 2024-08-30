0a82b549a81c1a989ea7ed0b7c2dbe2c02188206
jinweihuang20 <jinweihuang1020@gmail.com>
2024-08-30 14:54:50 +0800

    搬運模式(TransferTaskMode)切換為手動時不取消當前未完成的任務

--- Models/Sys/SystemModes.cs
+++ Models/Sys/SystemModes.cs
@@ -86,19 +86,19 @@ namespace AGVSystem.Models.Sys
                     _TransferTaskMode = value;
                     LOG.INFO($"Transfer Mode Changed to {_TransferTaskMode}");
                 }
-                if (_TransferTaskMode == TRANSFER_MODE.MANUAL)
-                {
-                    List<string> list_waiting_task = new List<string>();
-                    using (var db = new AGVSDatabase())
-                    {
-                        list_waiting_task = db.tables.Tasks.Where(tk => tk.State == TASK_RUN_STATUS.WAIT).Select(x => x.TaskName).ToList();
-                    }
-                    foreach (var task_name in list_waiting_task)
-                    {
-                        Task<bool> canceled = TaskManager.Cancel(task_name, $"Transfer Mode Changed to {_TransferTaskMode} canceled");
-                        canceled.Wait();
-                    }
-                }
+                //if (_TransferTaskMode == TRANSFER_MODE.MANUAL)
+                //{
+                //    List<string> list_waiting_task = new List<string>();
+                //    using (var db = new AGVSDatabase())
+                //    {
+                //        list_waiting_task = db.tables.Tasks.Where(tk => tk.State == TASK_RUN_STATUS.WAIT).Select(x => x.TaskName).ToList();
+                //    }
+                //    foreach (var task_name in list_waiting_task)
+                //    {
+                //        Task<bool> canceled = TaskManager.Cancel(task_name, $"Transfer Mode Changed to {_TransferTaskMode} canceled");
+                //        canceled.Wait();
+                //    }
+                //}
             }
         }
         public static bool RunModeSwitch(RUN_MODE mode, out string Message, bool forecing_change = false)
