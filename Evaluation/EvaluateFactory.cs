5373b5c76550769b125d5e3e71fe6bf97ffd94c3
jinweihuang20 <jinweihuang1020@gmail.com>
2024-09-03 21:42:14 +0800

    新增評估功能模型/加入AGVSystemTests測試專案

---
+++ Evaluation/EvaluateFactory.cs
@@ -0,0 +1,51 @@
+﻿using EquipmentManagment.Manager;
+
+namespace AGVSystem.Evaluation
+{
+    public static class EvaluateFactory
+    {
+        public static async Task StartTest()
+        {
+            EquipmentManagment.MainEquipment.clsEQ simEq = StaEQPManagager.MainEQList.Last();
+            EquipmentManagment.MainEquipment.clsEQ simEq2 = StaEQPManagager.MainEQList.Where(eq => eq != simEq).Last();
+            Evaluator evaluator = new Evaluator();
+            evaluator.EQStateKeyFrames = new()
+            {
+                 new KeyFrame(TimeSpan.FromSeconds(2))
+                 {
+                      EQControls = new()
+                      {
+                           new EQControl
+                           {
+                                EQ = simEq,
+                                 LdUldState = EQControl.LDULD_STATE.UNLOAD
+                           },
+                           new EQControl
+                           {
+                                EQ = simEq2,
+                                 LdUldState = EQControl.LDULD_STATE.LOAD
+                           }
+                      }
+                 },
+                 new KeyFrame(TimeSpan.FromSeconds(5))
+                 {
+                      EQControls = new()
+                      {
+                           new EQControl
+                           {
+                                EQ = simEq,
+                                 LdUldState = EQControl.LDULD_STATE.NO_REQUEST
+                           },
+                           new EQControl
+                           {
+                                EQ = simEq2,
+                                 LdUldState = EQControl.LDULD_STATE.UNLOAD
+                           }
+                      }
+                 }
+            };
+
+            await evaluator.Start();
+        }
+    }
+}
---
+++ Evaluation/Evaluator.cs
@@ -0,0 +1,55 @@
+﻿using EquipmentManagment.Manager;
+using NLog;
+
+namespace AGVSystem.Evaluation
+{
+    public class Evaluator
+    {
+        public List<KeyFrame> EQStateKeyFrames { get; set; } = new();
+        private Logger logger = LogManager.GetCurrentClassLogger();
+        public Evaluator()
+        {
+        }
+
+        public async Task Start()
+        {
+            //Use Task.Run to run the evaluation in a separate thread
+            await Task.Run(async () =>
+            {
+                logger.Info($"Evaluation Process Start");
+                //Loop through all the keyframes
+                TimeSpan lastTime = TimeSpan.Zero;
+                foreach (var _keyFrame in EQStateKeyFrames)
+                {
+                    TimeSpan timeDuration = (_keyFrame.Time - lastTime);
+                    lastTime = _keyFrame.Time;
+                    await Task.Delay(timeDuration);
+                    string _log = $"[{_keyFrame.Time}]";
+                    foreach (var eqControl in _keyFrame.EQControls)
+                    {
+                        EquipmentManagment.Emu.clsDIOModuleEmu eqEmulator = StaEQPEmulatorsManagager.GetEQEmuByName(eqControl.EQ.EndPointOptions.Name);
+                        //Check the state of the EQControl
+                        switch (eqControl.LdUldState)
+                        {
+                            case EQControl.LDULD_STATE.NO_REQUEST:
+                                eqEmulator.SetStatusBUSY();
+                                _log += $"\r\n--{eqControl.EQ.EndPointOptions.Name} is BUSY";
+                                break;
+                            case EQControl.LDULD_STATE.LOAD:
+                                eqEmulator.SetStatusLoadable();
+                                _log += $"\r\n--{eqControl.EQ.EndPointOptions.Name} is LOADABLE, ";
+                                break;
+                            case EQControl.LDULD_STATE.UNLOAD:
+                                eqEmulator.SetStatusUnloadable();
+                                _log += $"\r\n--{eqControl.EQ.EndPointOptions.Name} is UNLOADABLE";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    logger.Info(_log);
+                }
+            });
+        }
+    }
+}
