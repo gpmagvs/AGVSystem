90f6f83ae47e34372f07ffade5f518b9eed3080c
jinweihuang20 <jinweihuang1020@gmail.com>
2024-09-04 12:57:28 +0800

    新增第三方程式啟動(先加入了CIM) / 修改 nlog.config / Update wwwroot

---
+++ Service/ThirdPartyProgramStartService.cs
@@ -0,0 +1,46 @@
+﻿
+using AGVSystemCommonNet6.Sys;
+using System.Diagnostics;
+
+namespace AGVSystem.Service
+{
+    public class ThirdPartyProgramStartService : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _StartCIM();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+        }
+
+        private void _StartCIM()
+        {
+            try
+            {
+                string cimApp = EnvironmentVariables.GetUserVariable("GPM_CIM_Path");
+                if (string.IsNullOrEmpty(cimApp))
+                    return;
+                string appName = Path.GetFileNameWithoutExtension(cimApp);
+                Process[] pros = Process.GetProcessesByName(appName);
+
+                if (pros.Any())
+                    return;
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = false,
+                    WorkingDirectory = Path.GetDirectoryName(cimApp),
+                    FileName = cimApp,
+                    CreateNoWindow = false,
+                };
+                Process.Start(startInfo);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
