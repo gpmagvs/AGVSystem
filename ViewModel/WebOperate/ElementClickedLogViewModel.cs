38def722e7d0b3a0ce264f6ab1dbc06a8bafe5a5
jinweihuang20 <jinweihuang1020@gmail.com>
2024-10-22 12:45:48 +0800

    新增API : 前端用戶操作LOG

---
+++ ViewModel/WebOperate/ElementClickedLogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace AGVSystem.ViewModel
+{
+    public class ElementClickedLogViewModel
+    {
+        public string Name { get; set; } = "";
+        public string Source { get; set; } = "";
+    }
+}
