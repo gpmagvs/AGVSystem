2a1b15ce101a71aeb337eecfdedff80c06ad408c
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-08 17:23:54 +0800

    WIP控制器新增 API : GetAllSlotsOptions / 修改  EquipmentController.GetWIPOptions API

--- Controllers/WIPController.cs
+++ Controllers/WIPController.cs
@@ -1,4 +1,7 @@
-﻿using AGVSystem.Service;
+﻿using AGVSystem.Models.Map;
+using AGVSystem.Service;
+using AGVSystemCommonNet6.MAP;
+using EquipmentManagment.Device.Options;
 using EquipmentManagment.Manager;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +32,27 @@ namespace AGVSystem.Controllers
             //StaEQPManagager.WIPController.RemoveCargoID(WIPID, PortID);
             _rackControlService.RemoveRackCargoID(WIPID, PortID, this.GetType().Name);
             return Ok(new { confirm = true, message = "" });
+        }
+
+        [HttpGet("GetAllSlotsOptions")]
+        public async Task<IActionResult> GetAllSlotsOptions()
+        {
+            IEnumerable<int> tags = StaEQPManagager.RacksOptions.Values.SelectMany(opt => opt.ColumnTagMap.Values.SelectMany(tag => tag));
+            Dictionary<int, string[]> slotPortsNoDisplayMap = tags.ToDictionary(tag => tag, tag => GetPortNosOfRackColumn(tag));
+
+            string[] GetPortNosOfRackColumn(int tag)
+            {
+                clsRackOptions RackOption = StaEQPManagager.RacksOptions.Values.FirstOrDefault(opt => opt.ColumnTagMap.Values.Any(tags => tags.Contains(tag)));
+                if (RackOption != null)
+                {
+                    int column = RackOption.ColumnTagMap.ToList().FindIndex(pt => pt.Value.Contains(tag));
+                    IEnumerable<clsRackPortProperty> ports = RackOption.PortsOptions.Where(portOpt => portOpt.Column == column);
+                    string[] portNos = ports.OrderBy(port => port.Row).Select(col => col.PortNo).ToArray();
+                    return portNos;
+                }
+                return new string[3] { "0", "1", "2" };
+            }
+            return Ok(slotPortsNoDisplayMap);
         }
     }
 }
