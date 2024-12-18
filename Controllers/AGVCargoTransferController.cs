af4217089519fc751cdb29da3cf6838628fb25b8
jinweihuang20 <jinweihuang1020@gmail.com>
2024-12-19 02:23:29 +0800

    VMS上報貨物移出/移入 Port API Query參數新增 taskName ; 判斷VMS上報或物移入Port完成時，若該筆命令來源是AGV且命令之CarrierID包含UN或MI字串則將命令的CarrierID修改至Port上

--- Controllers/AGVCargoTransferController.cs
+++ Controllers/AGVCargoTransferController.cs
@@ -1,4 +1,5 @@
 ﻿using AGVSystem.Service;
+using AGVSystemCommonNet6.AGVDispatch;
 using AGVSystemCommonNet6.DATABASE;
 using AGVSystemCommonNet6.Microservices.MCS;
 using Microsoft.AspNetCore.Http;
@@ -25,7 +26,7 @@ namespace AGVSystem.Controllers
         /// <param name="slot"></param>
         /// <returns></returns>
         [HttpPost("UnloadCargoFromPort")]
-        public async Task<IActionResult> UnloadCargoFromPort(string agvName, int tagNumber, int slot)
+        public async Task<IActionResult> UnloadCargoFromPort(string taskName, string agvName, int tagNumber, int slot)
         {
             string removedCarrierID = string.Empty;
             (bool confirm, removedCarrierID, string message) = await _rackCargoStatusContorlService.RemoveRackCargoID(tagNumber, slot, this.GetType().Name, true);
@@ -39,12 +40,16 @@ namespace AGVSystem.Controllers
         /// <param name="slot"></param>
         /// <returns></returns>
         [HttpPost("LoadCargoToPort")]
-        public async Task<IActionResult> LoadCargoToPort(string agvName, int tagNumber, int slot, string cargoID = "")
+        public async Task<IActionResult> LoadCargoToPort(string taskName, string agvName, int tagNumber, int slot, string cargoID = "")
         {
             var agvState = dbContext.AgvStates.FirstOrDefault(agv => agv.AGV_Name == agvName);
             string agvID = agvState.AGV_ID;
+            clsTaskDto? order = dbContext.Tasks.AsNoTracking().FirstOrDefault(task => task.TaskName == taskName);
+            string cargoIDToLoad = cargoID;
+            if (order != null && order.soucePortID == agvID && (order.Carrier_ID.ToUpper().Contains("UN") || order.Carrier_ID.ToUpper().Contains("MI")))
+                cargoIDToLoad = order.Carrier_ID;
             //await MCSCIMService.CarrierRemoveCompletedReport(cargoID, agvID, "", 1);
-            await _rackCargoStatusContorlService.AddRackCargoID(tagNumber, slot, cargoID, this.GetType().Name, true);
+            await _rackCargoStatusContorlService.AddRackCargoID(tagNumber, slot, cargoIDToLoad, this.GetType().Name, true);
             return Ok();
         }
     }
