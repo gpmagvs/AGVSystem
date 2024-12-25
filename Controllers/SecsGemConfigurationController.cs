7c7ed29a59c66cf067d755b8a54f12488ad415d4
jinweihuang20 <jinweihuang1020@gmail.com>
2024-12-25 14:09:40 +0800

    新增 SecsGemConfigurationController 控制器

---
+++ Controllers/SecsGemConfigurationController.cs
@@ -0,0 +1,46 @@
+﻿using AGVSystemCommonNet6.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static AGVSystemCommonNet6.Microservices.MCSCIM.TransferReportConfiguration;
+
+namespace AGVSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecsGemConfigurationController : ControllerBase
+    {
+
+        private readonly SECSConfigsService service;
+        public SecsGemConfigurationController(SECSConfigsService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public async Task<SECSConfigsService> Get()
+        {
+            return service;
+        }
+
+        [HttpPost("saveReturnCodeSetting")]
+        public async Task<object> SaveReturnCodeSetting([FromBody] clsSaveReturnCodeRequest returnCodeSettings)
+        {
+            try
+            {
+                service.UpdateReturnCodes(returnCodeSettings.transferCompletedResultCodes);
+                return new { confirm = true, message = "" };
+            }
+            catch (Exception ex)
+            {
+                return new { confirm = false, message = ex.Message };
+            }
+        }
+
+
+        public class clsSaveReturnCodeRequest
+        {
+            public clsResultCodes transferCompletedResultCodes { get; set; } = new clsResultCodes();
+        }
+
+    }
+}
