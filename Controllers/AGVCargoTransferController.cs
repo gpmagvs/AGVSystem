4f679a521892be01704a689ab9c3c7924c29d4c1
jinweihuang20 <jinweihuang1020@gmail.com>
2024-12-25 08:10:12 +0800

    新增取放貨結束後設備狀態模擬(load->unload/ unload->load)

--- Controllers/AGVCargoTransferController.cs
+++ Controllers/AGVCargoTransferController.cs
@@ -2,6 +2,8 @@
 using AGVSystemCommonNet6.AGVDispatch;
 using AGVSystemCommonNet6.DATABASE;
 using AGVSystemCommonNet6.Microservices.MCS;
+using EquipmentManagment.Emu;
+using EquipmentManagment.Manager;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -29,8 +31,25 @@ namespace AGVSystem.Controllers
         public async Task<IActionResult> UnloadCargoFromPort(string taskName, string agvName, int tagNumber, int slot)
         {
             string removedCarrierID = string.Empty;
+            bool isEmuEqStation = IsSimulationEq(tagNumber, slot, out EQEmulatorBase emulator);
+            if (isEmuEqStation)
+            {
+                emulator.SetPortExist(0);
+                emulator.SetCarrierIDRead("");
+                await Task.Delay(300);
+            }
             (bool confirm, removedCarrierID, string message) = await _rackCargoStatusContorlService.RemoveRackCargoID(tagNumber, slot, this.GetType().Name, true);
+
+            if (isEmuEqStation)
+            {
+                _ = Task.Delay(100).ContinueWith((t) =>
+                {
+                    emulator.SetStatusLoadable();
+                });
+            }
+
             return Ok();
+
         }
 
         /// <summary>
@@ -42,6 +61,9 @@ namespace AGVSystem.Controllers
         [HttpPost("LoadCargoToPort")]
         public async Task<IActionResult> LoadCargoToPort(string taskName, string agvName, int tagNumber, int slot, string cargoID = "")
         {
+            bool isEmuEqStation = IsSimulationEq(tagNumber, slot, out EQEmulatorBase emulator);
+            if (isEmuEqStation)
+                emulator.SetPortExist(1);
             var agvState = dbContext.AgvStates.FirstOrDefault(agv => agv.AGV_Name == agvName);
             string agvID = agvState.AGV_ID;
             clsTaskDto? order = dbContext.Tasks.AsNoTracking().FirstOrDefault(task => task.TaskName == taskName);
@@ -50,7 +72,22 @@ namespace AGVSystem.Controllers
                 cargoIDToLoad = order.Carrier_ID;
             //await MCSCIMService.CarrierRemoveCompletedReport(cargoID, agvID, "", 1);
             await _rackCargoStatusContorlService.AddRackCargoID(tagNumber, slot, cargoIDToLoad, this.GetType().Name, true);
+            if (isEmuEqStation)
+                _ = Task.Delay(100).ContinueWith((t) =>
+                {
+                    emulator.SetStatusUnloadable();
+                });
             return Ok();
+        }
+
+
+        bool IsSimulationEq(int _tag, int _slot, out EQEmulatorBase emulator)
+        {
+            emulator = null;
+            var eq = StaEQPManagager.GetEQByTag(_tag, _slot);
+            if (eq != null && eq.EndPointOptions.IsEmulation)
+                emulator = StaEQPEmulatorsManagager.GetEQEmuByName(eq.EQName);
+            return emulator != null;
         }
     }
 }
