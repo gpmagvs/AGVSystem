c8124d64b7e127293a70fcdc94606182174be001
jinweihuang20 <jinweihuang1020@gmail.com>
2024-12-25 09:24:06 +0800

    前端signalR 連線後僅傳送消息給當前連線者 / signalR客戶端連線後發送資料庫狀態

--- Hubs/FrontEndDataHub.cs
+++ Hubs/FrontEndDataHub.cs
@@ -1,16 +1,24 @@
 ﻿using AGVSystem.Service;
+using AGVSystem.ViewModel;
+using AGVSystemCommonNet6.DATABASE;
 using Microsoft.AspNetCore.SignalR;
+using NuGet.Protocol.Plugins;
 using System.Collections.Concurrent;
 
 public class FrontEndDataHub : Hub
 {
+    AGVSDbContext dbContext;
+    public FrontEndDataHub(AGVSDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
 
     public static ConcurrentDictionary<string, IClientProxy> connectedClients = new ConcurrentDictionary<string, IClientProxy>();
 
     public override async Task OnConnectedAsync()
     {
         connectedClients.TryAdd(Context.ConnectionId, Clients.User(Context.ConnectionId));
-        await Clients.All.SendAsync("ReceiveData", "VMS", FrontEndDataBrocastService._previousData);
+        SendDataOnConnected();
         await base.OnConnectedAsync();
 
     }
@@ -29,5 +37,24 @@ public class FrontEndDataHub : Hub
             await Clients.All.SendAsync("ReceiveData", "VMS", FrontEndDataBrocastService._previousData);
         }
         //return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    private async Task SendDataOnConnected()
+    {
+        try
+        {
+            await Clients.Caller.SendAsync("ReceiveData", "VMS", FrontEndDataBrocastService._previousData);
+            using (AGVSDatabase dbHelper = new AGVSDatabase())
+            {
+                if (!AGVSDatabase.CheckPrimaryKeys(dbHelper, out List<string> primaryErrorTables))
+                {
+                    await Clients.Caller.SendAsync("DatabaseState", new DatabaseState() { state = false, message = $"資料表 [{string.Join(",", primaryErrorTables)}] 需添加主鍵" });
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message + ex.StackTrace);
+        }
     }
 }
---
+++ ViewModel/DatabaseState.cs
@@ -0,0 +1,9 @@
+﻿namespace AGVSystem.ViewModel
+{
+    public class DatabaseState
+    {
+        public bool state { get; set; } = true;
+
+        public string message { get; set; } = string.Empty;
+    }
+}
