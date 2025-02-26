128f6cd6581748f2a2c2f852bb2c8a367116c7e3
jinweihuang20 <jinweihuang1020@gmail.com>
2025-02-26 17:30:22 +0800

    update prbuild.bat

--- prebuild.bat
+++ prebuild.bat
@@ -1,3 +1,4 @@
+cd /d "%~dp0"
 robocopy wwwroot bin\Debug\net6.0\wwwroot /MIR /MT:10
 robocopy wwwroot bin\Release\net6.0\wwwroot /MIR /MT:10
 robocopy wwwroot_beta bin\Debug\net6.0\wwwroot_beta /MIR /MT:10
